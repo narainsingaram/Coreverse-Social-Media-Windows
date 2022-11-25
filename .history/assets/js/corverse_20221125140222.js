f7708fcc3fc6f2bc3ba2112740aaa0f427cb9e63
Narain Singaram <narain.awesome@gmail.com>
2022-11-25 14:03:16 -0500

    nvm

---
+++ .history/assets/js/corverse_20221125140222.js
@@ -0,0 +1,74 @@
+// function getUsers(value, user) {
+// 	$.post("includes/handlers/ajax_friend_search.php", {query:value, userLoggedIn:user}, function(data) {
+// 		$(".results").html(data);
+// 	});
+// }
+
+function getDropdownData(user, type) {
+
+	if($(".dropdown_data_window").css("height") == "0px") {
+
+		var pageName;
+
+		if(type == 'notification') {
+			pageName = "ajax_load_notifications.php";
+			$("span").remove("#unread_notification");
+		}
+		else if (type == 'message') {
+			pageName = "ajax_load_messages.php";
+			$("span").remove("#unread_message");
+		}
+
+		var ajaxreq = $.ajax({
+			url: "includes/handlers/" + pageName,
+			type: "POST",
+			data: "page=1&userLoggedIn=" + user,
+			cache: false,
+
+			success: function(response) {
+				$(".dropdown_data_window").html(response);
+				$(".dropdown_data_window").css({"padding" : "0px", "height": "fit-content"});
+				$("#dropdown_data_type").val(type);
+			}
+
+		});
+
+	}
+	else {
+		$(".dropdown_data_window").html("");
+		$(".dropdown_data_window").css({"padding" : "0px", "height": "0px", "border" : "none"});
+	}
+
+}
+
+	function getLiveSearchUsers(value, user) {
+	
+		$.post("includes/handlers/ajax_search.php", {query:value, userLoggedIn: user}, function(data) {
+	
+			if($(".search_results_footer_empty")[0]) {
+				$(".search_results_footer_empty").toggleClass("search_results_footer");
+				$(".search_results_footer_empty").toggleClass("search_results_footer_empty");
+			}
+	
+			$('.search_results').html(data);
+			$('.search_results_footer').html("<a class='view_results_text' href='search.php?q=" + value + "'>View all results</a>");
+	
+			if(value.length == 0) {
+				$('.search_results_footer').html("");
+				$('.search_results_footer').toggleClass("search_results_footer_empty");
+				$('.search_results_footer').toggleClass("search_results_footer");
+			}
+	
+		});
+	}
+
+ 
+$(function(){
+	$("#submitGallery").on("click", function(e){
+		if($("#galleryUpload").val() === "") {
+			e.preventDefault();
+			confirm("Please select an image first");
+			return;
+		}
+	})
+})
